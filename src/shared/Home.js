b4b481977865aa3fa188671aa2cdfd909fd37604
lssuseendharlal <lssuseendharlal@gmail.com>
2020-05-02 18:34:26 +0530

    theme added

--- src/shared/Home.js
+++ src/shared/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const home = () => {
   return (
     <div>
-      <h1>Welcome to Recipe Book!</h1>
+      <h1 style={{ color: " var(--primaryText)" }}>Welcome to Recipe Book!</h1>
       <a href="https://github.com/suseendharlal96/recipe-react">
         Github for recipe
       </a>
